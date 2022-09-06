c5f21fd50e13ab815433413d31fb1a33b189b193
Elton <elton.fontes@petlove.com.br>
2022-09-06 09:41:16 -0300

    refactor: separated into functions

--- src/main/kotlin/br/com/petlove/Main.kt
+++ src/main/kotlin/br/com/petlove/Main.kt
@@ -1,11 +1,71 @@
 package br.com.petlove // ktlint-disable filename
-import br.com.petlove.exception.EncrypterException
-import org.json.JSONException
-import org.json.JSONObject
-import java.text.SimpleDateFormat
+import com.adyen.Client
+import com.adyen.enums.Environment
+import com.adyen.model.Amount
+import com.adyen.model.checkout.PaymentsRequest
+import com.adyen.model.checkout.PaymentsResponse
+import com.adyen.model.checkout.details.CardDetails
+import com.adyen.service.Checkout
+import com.adyen.service.exception.ApiException
 import java.util.*
 
 fun main() {
+    validateRawCardData()
+    // testCardEncryption()
+}
+
+fun validateRawCardData() {
+    val xApiKey = System.getenv("ADYEN_API_KEY")
+    val merchantAccount = System.getenv("ADYEN_MERCHANT_ACCOUNT")
+    val sendCardEncrypted = true
+
+    try {
+        val response: PaymentsResponse = Checkout(
+            Client(xApiKey, Environment.TEST)
+        )
+            .payments(
+                PaymentsRequest()
+                    .merchantAccount(merchantAccount)
+                    .enableRecurring(true)
+                    .amount(
+                        Amount()
+                            .currency("BRL")
+                            .value(0L)
+                    )
+                    .paymentMethod(
+                        details(sendCardEncrypted)
+                    )
+                    .recurringProcessingModel(PaymentsRequest.RecurringProcessingModelEnum.SUBSCRIPTION)
+                    .shopperInteraction(PaymentsRequest.ShopperInteractionEnum.CONTAUTH)
+                    .reference("PAY-321654987")
+            )
+        println("Success: $response")
+    } catch (ex: ApiException) {
+        println("Error Api: ${ex.localizedMessage}")
+    } catch (ex: Exception) {
+        println("Error: ${ex.localizedMessage}")
+    }
+}
+
+fun details(encrypted: Boolean = false): CardDetails =
+    if (encrypted) {
+        CardDetails()
+            .type("scheme")
+            .encryptedCardNumber("test_4111111111111111")
+            .encryptedExpiryMonth("test_03")
+            .encryptedExpiryYear("test_2030")
+            .encryptedSecurityCode("test_737")
+    } else {
+        CardDetails()
+            .type("scheme")
+            .number("4111111111111111")
+            .expiryMonth("03")
+            .expiryYear("2030")
+            .holderName("John Smith")
+            .cvc("737")
+    }
+
+fun testCardEncryption() {
     val publicKey = System.getenv("ADYEN_PUBLIC_KEY")
 
     try {
@@ -36,77 +96,5 @@ fun main() {
         println("Expiry Year: $expiryYear")
     } catch (ex: Exception) {
         println(ex.localizedMessage)
-    }
-}
-
-class Card(private var publicKey: String) {
-    var number: String? = null
-    var expiryMonth: String? = null
-    var expiryYear: String? = null
-    var cardHolderName: String? = null
-    var cvc: String? = null
-    var generationTime: Date? = null
-
-    fun serialize(): String? {
-        val cardJson = JSONObject()
-        var encryptedData: String? = null
-        try {
-            cardJson.put("generationtime", GENERATION_DATE_FORMAT.format(generationTime))
-            cardJson.put("number", number)
-            cardJson.put("holderName", cardHolderName)
-            cardJson.put("cvc", cvc)
-            cardJson.put("expiryMonth", expiryMonth)
-            cardJson.put("expiryYear", expiryYear)
-            encryptedData = encryptData(cardJson.toString(), publicKey)
-        } catch (e: JSONException) {
-            println(e.message)
-        }
-        return encryptedData
-    }
-
-    fun serializeParam(param: String, value: String): String? {
-        val cardJson = JSONObject()
-        var encryptedData: String? = null
-        try {
-            cardJson.put("generationtime", GENERATION_DATE_FORMAT.format(generationTime))
-            cardJson.put(param, value)
-            encryptedData = encryptData(cardJson.toString(), publicKey)
-        } catch (e: JSONException) {
-            println(e.message)
-        }
-        return encryptedData
-    }
-
-    private fun encryptData(data: String, publicKey: String): String? {
-        var encryptedData: String? = null
-        try {
-            val encrypter = ClientSideEncrypter(publicKey)
-            encryptedData = encrypter.encrypt(data)
-        } catch (e: EncrypterException) {
-            println("Error in encrypter " + e.message)
-        }
-        return encryptedData
-    }
-
-    override fun toString(): String {
-        val cardJson = JSONObject()
-        try {
-            cardJson.put("generationtime", GENERATION_DATE_FORMAT.format(generationTime))
-            if (number!!.length >= 4) {
-                cardJson.put("number", number!!.substring(0, 3))
-            }
-            cardJson.put("holderName", cardHolderName)
-        } catch (e: JSONException) {
-            println(e.message)
-        }
-        return cardJson.toString()
-    }
-
-    companion object {
-        private val GENERATION_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-
-        init {
-            GENERATION_DATE_FORMAT.timeZone = TimeZone.getTimeZone("UTC")
-        }
     }
 }
